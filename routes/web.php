c839af21ee4c7cd6f7b166083272786c70888ea2
arta-ademaj <85793710+arta-ademaj@users.noreply.github.com>
2023-11-08 23:03:01 +0100

    first commit
    first commit

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AdminController;
-use App\Http\Controllers\SchoolController;
-use App\Http\Controllers\SchoolUserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,17 +16,3 @@ use App\Http\Controllers\SchoolUserController;
 Route::get('/', function () {
     return view('welcome');
 });
-
-
-
-
-
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/admin', [App\Http\Controllers\AdminController::class, 'index'])->name('admin')->middleware('admin');
-Route::resource('schools',  SchoolController::class);
-Route::resource('schools-users',  SchoolUserController::class);
-
-
-
-Auth::routes();
